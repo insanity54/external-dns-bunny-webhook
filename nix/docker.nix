b99ee78db80992db481f8e1220fa45ec8ecd41c4
Asherah Connor <ashe@kivikakk.ee>
2025-08-18 15:26:23 +1000

    nix/docker: (experimental) support docker build on mac.
    We can't build layered images, so just build a regular one.

--- nix/docker.nix
+++ nix/docker.nix
@@ -1,22 +1,27 @@
 {
+  lib,
+  system,
   dockerTools,
   external-dns-bunny-webhook,
   external-dns-bunny-webhook-rev,
   ...
 }:
-dockerTools.streamLayeredImage {
-  name = "nossa.ee/talya/external-dns-bunny-webhook";
-  tag = external-dns-bunny-webhook-rev;
-  config = {
-    Cmd = [ "/bin/webhook" ];
-    User = "1000:1000";
+let
+  spec = {
+    name = "nossa.ee/talya/external-dns-bunny-webhook";
+    tag = external-dns-bunny-webhook-rev;
+    config = {
+      Cmd = [ "/bin/webhook" ];
+      User = "1000:1000";
+    };
+
+    contents = [
+      external-dns-bunny-webhook
+      dockerTools.caCertificates
+    ];
   };
-
-  contents = [
-    external-dns-bunny-webhook
-    dockerTools.binSh
-    dockerTools.caCertificates
-  ];
-
-  maxLayers = 120;
-}
+in
+if builtins.elem system lib.platforms.darwin then
+  dockerTools.buildImage spec
+else
+  dockerTools.streamLayeredImage (spec // { maxLayers = 120; })
